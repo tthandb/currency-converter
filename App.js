cc6c593651d9a011ae1e2c11a016bb97c1e94772
tthandb <tthandb@hotmail.com>
2020-07-13 21:54:55 +0700

    outline little

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Please enter the value of the currency you want to convert</Text>
+      <TouchableOpacity style={styles.convertBtn}>
+        <Text style={styles.labelBtn}>VND to USD</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.convertBtn}>
+        <Text style={styles.labelBtn}>VND to USD</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  convertBtn: {},
+  labelBtn: {},
 });
